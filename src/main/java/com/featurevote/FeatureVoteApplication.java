0865503e7808bb646dee3a17051a967b6525d2c4
Kevin <kevinchan246@gmail.com>
2020-03-15 00:19:48 -0500

    added the servlet initializer and renamed the application file

---
+++ src/main/java/com/featurevote/FeatureVoteApplication.java
@@ -0,0 +1,13 @@
+package com.featurevote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureVoteApplication.class, args);
+	}
+
+}
