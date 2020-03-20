eedb1eb227442c5ca9dee534469e07aae75d3952
Kevin <kevinchan246@gmail.com>
2020-03-20 09:24:28 -0500

    added the password encoder, dashboard page, updated web security config

--- src/main/java/com/featurevote/security/WebSecurityConfiguration.java
+++ src/main/java/com/featurevote/security/WebSecurityConfiguration.java
@@ -1,26 +1,35 @@
 package com.featurevote.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication() // not recommmended to use this in production
+            .passwordEncoder(getPasswordEncoder())
             .withUser("testing@gmail.com")
-            .password("1234567")
+            .password(getPasswordEncoder().encode("1234567"))                        //since using the encoder above, need to make sure that this password is also encoded.
             .roles("USER");
     }
 
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .anyRequest().hasRole("USER").and()
+                .antMatchers("/").permitAll()   //permit all to see the root page
+                .antMatchers("/login").permitAll()  //permit all to see the login page
+                .anyRequest().hasRole("USER").and() //all other pages has to have a "USER" role
             .formLogin()
                 .loginPage("/login")
                 .permitAll()
--- src/main/java/com/featurevote/web/DashboardController.java
+++ src/main/java/com/featurevote/web/DashboardController.java
@@ -1,13 +1,22 @@
 package com.featurevote.web;
 
         import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DashboardController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+
+
+    @GetMapping("/")                //mapping to root page, same thing as the above line
     public String rootView(){
-        return "index";
+        return "index";             //returning the the page mapped, root page in this case
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
     }
 }
